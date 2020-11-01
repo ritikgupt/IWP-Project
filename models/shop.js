8f7fbb9c5b1baf3bb0c2e2ae9ca3f7c78c30724b
Ritik <ritikgupta89369@gmail.com>
2020-11-01 11:35:37 +0530

    Final changes pushed

--- models/shop.js
+++ models/shop.js
@@ -1,8 +1,8 @@
 const c = require('mongoose');
 const ShopSchema = new c.Schema({
-  title: String,
-  image: String,
-  body: String,
+  title: {type:String,required:true},
+  image: {type:String,required:true},
+  body: {type:String,required:true},
   id: String,
   user:{
     type:c.Schema.ObjectId,
--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const c = require('mongoose');
 const h = require('passport-local-mongoose');
 const UserSchema = new c.Schema({
-  username: String,
-  password: String,
-  email: String,
-  mobile: String,
-  address: String,
+  username: {type:String,required:true,unique:true},
+  password: {type:String,required:true},
+  email: {type:String,required:true,unique:true},
+  mobile: {type:String,required:true},
+  address: {type:String,required:true},
 });
 UserSchema.plugin(h);
 module.exports = c.model('User', UserSchema);
